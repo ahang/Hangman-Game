3021409c0633ca9fb44ada0411c265214d0108f7
dashaman <hang.andy.t@gmail.com>
2017-02-12 15:13:42 -0600

    v0.4

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,27 +1,43 @@
 //Code should be exectuted in strict mode
 "use strict";
 
-//List Array of Song Artists from Guardians of the Galaxy that we will select from
-var artists = ["Blue Swede", "Raspberries", "Greenbaum", "David Bowie",
-            "Elvin Bishop", "10cc", "Jackson 5", "Redbone", "The Runaways",
-            "Rupert Holmes", "Stairsteps", "Marvine Gaye and Tammi Terrell"];
-//List array of valid letters
-var validLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-                    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
-                    "x", "y", "z"];
-var guesses = [];
+//Global Variables
+var wins = 0;
+
+var lives = 10;
+var userGuess = [];
+var correctGuesses = [];
 var lives;
 
+//List Array of Song Artists from Guardians of the Galaxy that we will select from
+var characters = ["Genji", "Mccree", "Pharah", "Reaper", "Soldier76", "Sombra", "Tracer",
+                "Bastion", "Hanzo"," Junkrat", "Mei", "Torbjorn", "Widowmaker", "Dva",
+                "Reinhardt", "Roadhog", "Winston", "Zarya", "Ana", "Lucio", "Mercy",
+                "Symmetra", "Zenyatta"];
+//console.log(characters.length);
 
-//Variable to generate random artists
-function randomArtists() {
-    return artists[Math.floor(Math.random() * artists.length)];
+//Variable to generate random artists and
+var randomCharacters = characters[Math.floor(Math.random() * characters.length)];
+var splitCharacters = randomCharacters.split("");
+//Replace selected word with an "_"
+for (var i = 0; i < splitCharacters; i++) {
+    splitCharacters[i] = "_";
 }
-
-
+console.log(splitCharacters);
 
 //Track user input as well as error catch
 document.onkeyup = function(event) {
     var userInput = String.fromCharCode(event.keyCode).toLowerCase();
+    userGuess.push(userInput);
+// Compare user input with characters and either reveal letter or lose one life
+    for (var i = 0; i < randomCharacters; i++) {
+        if (randomCharacters.charAt(i) === userInput) {
+            correctGuesses.push(userInput);
+            splitCharacters.splice(i, 1, userInput);
+        } else if (randomCharacters.charAt(i) !== userInput) {
+            lives--;
+        }
+
+    }
 }
 
