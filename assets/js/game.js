f68e513a14a5c55860a6bb1300dc06c48657ed12
dashaman <hang.andy.t@gmail.com>
2017-02-12 22:17:53 -0600

    v0.5

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,43 +1,108 @@
 //Code should be exectuted in strict mode
 "use strict";
 
-//Global Variables
-var wins = 0;
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//               VARIABLES            ~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+var characters = ["GENJI", "MCCREE", "PHARAH", "REAPER",
+                    "SOLIDER76", "SOMBRA", "TRACER",
+                "BASTION", "HANZO","JUNKRAT", "MEI",
+                "TORBJORN", "WIDOWMAKER", "DVA",
+                "REINHARDT", "ROADHOG", "WINSTON",
+                "ZARYA", "ANA", "LUCIO", "MERCY",
+                "SYMMETRA", "ZENYATTA"];
+var randomCharacters = "";
+var hiddenCharacters = [];
+var numBlanks = 0;
+var blanks = [];
+var wrongGuesses = [];
+var userInput = "";
 
 var lives = 10;
-var userGuess = [];
-var correctGuesses = [];
-var lives;
+var wins = 0;
+var loss = 1;
 
-//List Array of Song Artists from Guardians of the Galaxy that we will select from
-var characters = ["Genji", "Mccree", "Pharah", "Reaper", "Soldier76", "Sombra", "Tracer",
-                "Bastion", "Hanzo"," Junkrat", "Mei", "Torbjorn", "Widowmaker", "Dva",
-                "Reinhardt", "Roadhog", "Winston", "Zarya", "Ana", "Lucio", "Mercy",
-                "Symmetra", "Zenyatta"];
-//console.log(characters.length);
 
-//Variable to generate random artists and
-var randomCharacters = characters[Math.floor(Math.random() * characters.length)];
-var splitCharacters = randomCharacters.split("");
-//Replace selected word with an "_"
-for (var i = 0; i < splitCharacters; i++) {
-    splitCharacters[i] = "_";
-}
-console.log(splitCharacters);
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//               FUNCTIONS            ~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/* Starts the game by setting everything to the default values
+Generates a random word and replaces them with underscores
+*/
 
-//Track user input as well as error catch
-document.onkeyup = function(event) {
-    var userInput = String.fromCharCode(event.keyCode).toLowerCase();
-    userGuess.push(userInput);
-// Compare user input with characters and either reveal letter or lose one life
-    for (var i = 0; i < randomCharacters; i++) {
-        if (randomCharacters.charAt(i) === userInput) {
-            correctGuesses.push(userInput);
-            splitCharacters.splice(i, 1, userInput);
-        } else if (randomCharacters.charAt(i) !== userInput) {
-            lives--;
-        }
+function startGame() {
+    wrongGuesses = [];
+    console.log("This is the # of wrong guesses in startGame", wrongGuesses);
+    lives = 10;
+    blanks = [];
 
+    randomCharacters = characters[Math.floor(Math.random() * characters.length)];
+    hiddenCharacters = randomCharacters.split("");
+    numBlanks = hiddenCharacters.length;
+    console.log(randomCharacters);
+    console.log(numBlanks);
+
+    for (var i = 0; i < numBlanks; i++) {
+        blanks.push("_");
     }
 }
+console.log(blanks);
+document.getElementById("word-blank").innerHTML = blanks.join(" ");
+document.getElementById("guesses-left").innerHTML = lives;
+/* Compares the letter user picked with letters of the selected word. Also,
+decreases the user lives by one if they guessed incorrectly and display it on
+the appropriate line in HTML
+TO DO - Prevent user from typing same letter twice*/
+function comparesLetters(letter) {
+    var letterInWord = false;
 
+    for (var i = 0; i < numBlanks; i++) {
+        if(randomCharacters[i] === letter) {
+            letterInWord = true;
+        }
+    }
+
+    if (letterInWord) {
+        for(i = 0; i < numBlanks; i++) {
+            if(randomCharacters[i] === letter) {
+                blanks[i] = letter;
+            }
+        }
+    } else {
+        lives --;
+        wrongGuesses.push(letter)
+    }
+
+}
+/*Updates the HTML with letters that are in the word, guess that are left,
+and show the wrong guesses. Also, determine whether the user is victorious*/
+function roundComplete() {
+    document.getElementById("word-blank").innerHTML = blanks.join(" ");
+    document.getElementById("guesses-left").innerHTML = lives;
+    document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+
+    console.log(hiddenCharacters);
+    console.log(blanks);
+    if(hiddenCharacters.join(" ") === blanks.join(" ")){
+        wins++;
+        document.getElementById("win-counter").innerHTML = wins;
+        startGame();
+    } else if(lives === 0) {
+        document.getElementById("loss-counter").innerHTML = loss ++;
+        document.getElementById("wrong-guesses").innerHTML = "";
+        alert("Game Over");
+        startGame();
+    }
+}
+//Starts the game!
+startGame();
+/*Take in the letter the user type in and pass it through the compareLetters
+function
+*/
+
+document.onkeyup = function(event) {
+    userInput = String.fromCharCode(event.keyCode).toUpperCase();
+    console.log("This is the letter we are typing", userInput);
+    comparesLetters(userInput);
+    roundComplete();
+}
